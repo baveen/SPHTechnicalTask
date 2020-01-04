b33025276bb4ad9183278535b6dbdc3fd5ccf97a
Baveendran Nagendran <baveen0704@gmail.com>
2020-01-04 14:42:19 +0800

    Initial Commit

---
+++ SPHTechnicalTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPHTechnicalTask
+//
+//  Created by Baveendran Nagendran on 1/4/20.
+//  Copyright © 2020 rbtechsolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
